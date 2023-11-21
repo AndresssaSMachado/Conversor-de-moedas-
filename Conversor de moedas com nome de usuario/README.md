7ba6a9722207cda38f625d5cd4fbbf2a81c98f8e
Andressa <146253485+AndresssaSMachado@users.noreply.github.com>
2023-11-21 13:05:25 -0300

    Add files via upload

---
+++ Conversor de moedas com nome de usuario/README.md
@@ -0,0 +1,4 @@
+# _Calculadora de média - Nome de usuário
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/AndressaSMachado/pen/oNmevgN](https://codepen.io/AndressaSMachado/pen/oNmevgN).
+
---
+++ Conversor de moedas com nome de usuario/src/script.js
@@ -0,0 +1,8 @@
+var valorEmDolar = 415.0;
+var cotacaoDoDolar = 4.92;
+var Lucas = "Lucas"; // Substitua com o nome de usuário desejado
+
+var valorEmReal = valorEmDolar * cotacaoDoDolar;
+valorEmReal = valorEmReal.toFixed(2);
+
+alert("Olá, " + Lucas + "! O valor será: R$" + valorEmReal);
